f2f689c8f99d6bd50267ad00965f2f0bca49716b
Soufiane Zeniou <szeniou@iMac-de-Soufiane.local>
2017-12-01 14:55:58 +0100

    refactor/#358: refactor currencyUI.js

--- src/components/consoleUI/consoleUI.js
+++ src/components/consoleUI/consoleUI.js
@@ -4,7 +4,7 @@ FSBL.addEventListener('onReady', function () {
 
 	FSBL.Clients.RouterClient.transmit("whenReadyChannel", "iAmReady");
 
-	//Listner for first data snapshot
+	//listener for first data snapshot
 	FSBL.Clients.RouterClient.addListener("FirstDataSnapshotChannel", function (error, response) {
 		if (error) {
 			document.getElementById("displayLogData").innerHTML = '';
@@ -12,14 +12,14 @@ FSBL.addEventListener('onReady', function () {
 		} else {
 
 			currencySymbol = response.data[0].Symbol;
-			document.getElementById("displayLogData").appendChild(document.createElement('h3')).innerHTML = "&nbsp;INITIAL DATA SNAPSHOT";
-			console.log("FIRST SNAPSHOT : " + response);
+			document.getElementById("displayLogData").appendChild(document.createElement('h3')).innerHTML = "INITIAL DATA SNAPSHOT";
+			console.log("FirstDataSnapshotChannel : " + response);
 			var firstDataSnapshot = response.data[0];
 			output(syntaxHighlight(JSON.stringify(firstDataSnapshot)));
 		}
 	});
 
-	//listner for patchs
+	//listener for patchs
 	FSBL.Clients.RouterClient.addListener("PatchChannel", function (error, response) {
 		if (error) {
 			document.getElementById("displayLogData").innerHTML = '';
@@ -33,11 +33,11 @@ FSBL.addEventListener('onReady', function () {
 		}
 	});
 
-	//Listner for errors 
+	//listener for errors 
 	FSBL.Clients.RouterClient.addListener("ErrorMessageChannel", function (error, response) {
 		if (error) {
 			document.getElementById("displayLogData").innerHTML = '';
-			console.log("SDFINSEMBLECHANNELc: " + JSON.stringify(error));
+			console.log("ErrorMessageChannel: " + JSON.stringify(error));
 		} else {
 			document.getElementById("displayLogData").appendChild(document.createElement('h3')).innerHTML = "  ERROR ";
 			var errormssg = response.data;
@@ -47,13 +47,13 @@ FSBL.addEventListener('onReady', function () {
 	});
 
 	/* 
-    For closing this window after closing the source window
+    For closing this window after closing the main window
     */
 	FSBL.Clients.RouterClient.addListener("closeMainWindowChannel", function (error, response) {
 		if (error) {
-			console.log("closeMainWindowChannel Error: " + JSON.stringify(error));
+			console.log("closeMainWindowChannel error: " + JSON.stringify(error));
 		} else {
-			console.log("closeMainWindowChannel Response: " + JSON.stringify(response));
+			console.log("closeMainWindowChannel: " + JSON.stringify(response));
 			console.log("Closing windiow");
 			window.close();
 		}
@@ -61,14 +61,15 @@ FSBL.addEventListener('onReady', function () {
 });
 
 //For displaying in the UI
-function output(inp) {
-	document.getElementById("displayLogData").appendChild(document.createElement('p')).innerHTML = inp;
+function output(data) {
+	document.getElementById("displayLogData").appendChild(document.createElement('p')).innerHTML = data;
 }
 
 //For a better display
 function syntaxHighlight(json) {
 	json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+		//cls is an abbreviation of the keyword class
 		var cls = 'number';
 		if (/^"/.test(match)) {
 			if (/:$/.test(match)) {
